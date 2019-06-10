a7c895499627f6765d91430a0dcf53d63aebd534
irinamontgomery <irina.montgomery@gmail.com>
2019-06-10 18:16:41 -0500

    Updates

---
+++ precache-manifest.c247d1fbb9b700e8408c9471a2d45952.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3199711b7c6dff643e5207ad93cd0952",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "b91eec48d3ef55893c2d",
+    "url": "/clicky-game/static/css/main.def86449.chunk.css"
+  },
+  {
+    "revision": "b91f66e48ecaac6f4e8a",
+    "url": "/clicky-game/static/js/2.adc47061.chunk.js"
+  },
+  {
+    "revision": "b91eec48d3ef55893c2d",
+    "url": "/clicky-game/static/js/main.35ab771f.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  },
+  {
+    "revision": "caf821c07b12d196aba89866f160b695",
+    "url": "/clicky-game/static/media/bird.caf821c0.jpg"
+  },
+  {
+    "revision": "7a77e30539761ac28fb3b3587eae0a00",
+    "url": "/clicky-game/static/media/cat.7a77e305.jpg"
+  },
+  {
+    "revision": "71c6365b336b927f6941b3e1577beff6",
+    "url": "/clicky-game/static/media/cheetah.71c6365b.jpg"
+  },
+  {
+    "revision": "2f37093b1bf9b6d173bedd07f378c45a",
+    "url": "/clicky-game/static/media/dog.2f37093b.jpg"
+  },
+  {
+    "revision": "3ea15f184e75956cba09fe257b859890",
+    "url": "/clicky-game/static/media/elephant.3ea15f18.jpg"
+  },
+  {
+    "revision": "425b4f50cf21a6e916c903efbd87fc17",
+    "url": "/clicky-game/static/media/fox.425b4f50.jpg"
+  },
+  {
+    "revision": "c7a98160f3d5a28930bdc58574d41f2f",
+    "url": "/clicky-game/static/media/giraffe.c7a98160.jpg"
+  },
+  {
+    "revision": "5bad9cd2609e11e00dc22610df118c50",
+    "url": "/clicky-game/static/media/hamster.5bad9cd2.jpg"
+  },
+  {
+    "revision": "050de0cba798680278b07c5058057119",
+    "url": "/clicky-game/static/media/horse.050de0cb.jpg"
+  },
+  {
+    "revision": "4cefd23a7fdc59ac022cc46d44fe9321",
+    "url": "/clicky-game/static/media/lion.4cefd23a.jpg"
+  },
+  {
+    "revision": "ebf686a0467fa8088a09ee6b90962b1f",
+    "url": "/clicky-game/static/media/turtle.ebf686a0.jpg"
+  },
+  {
+    "revision": "487e6e0dc5807cce342e49151542126b",
+    "url": "/clicky-game/static/media/zebra.487e6e0d.jpg"
+  }
+]);
